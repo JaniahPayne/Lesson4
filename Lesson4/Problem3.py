6e782f18db3ad2570405bc489e5c34c36aa7fdd1
JaniahPayne <44148033+JaniahPayne@users.noreply.github.com>
2018-12-10 13:10:11 -0500

    Add files via upload

---
+++ Lesson4/Problem3.py
@@ -0,0 +1,6 @@
+english = 89
+math = 95
+history = 87 
+science = 84 
+production = 80 
+print (english +math + history + science + production )
---
+++ Lesson4/problem1/python2.py
@@ -0,0 +1,4 @@
+score=3.8
+new_score = (score / 4) * 100
+new_score = round(new_score)
+print(new_score)
